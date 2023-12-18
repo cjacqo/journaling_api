b7c507118986ed3320b43ac5ef574fd6388ba1ac
cjacqo <76092910+cjacqo@users.noreply.github.com>
2023-12-18 15:04:06 -0800

    Made request to get user by username

--- index.js
+++ index.js
@@ -91,6 +91,20 @@ app.get('/users', async (req, res) => {
     })
 })
 
+/**
+ * GET A USER BY USERNAME
+ */
+app.get('/users/:UserName', async (req, res) => {
+  await Users.findOne({ UserName: req.params.UserName })
+    .then((user) => {
+      res.json(user)
+    })
+    .catch((err) => {
+      console.error(err)
+      res.status(500).send('Error: ' + err)
+    })
+})
+
 // list for requests
 app.listen(8080, () => {
   console.log('Your app is listening on port 8080')
