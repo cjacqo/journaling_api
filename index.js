7a5b42f41988e0a1ea55cfb1fc661b9ab666fa2b
cjacqo <76092910+cjacqo@users.noreply.github.com>
2023-12-18 14:46:25 -0800

    Installed cors and setup app to use bodyParser. Reorganized index.js. Now, creating a new user route works as expected

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
-const express = require('express')
-const app = express()
-const morgan = require('morgan')
 const mongoose = require('mongoose')
 const Models = require('./models.js')
-const fs = require('fs')
-const path = require('path')
-
 const Entries = Models.Entry
 const Users = Models.User
 
-mongoose.connect('mongodb://localhost:27017/journaling', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://127.0.0.1/journaling', { useNewUrlParser: true, useUnifiedTopology: true })
+
+const express = require('express'),
+app = express(),
+bodyParser = require('body-parser'),
+path = require('path'),
+morgan = require('morgan')
+
+app.use(morgan('common'))
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.json())
+
+const cors = require('cors')
+let allowedOrigins = [
+  'https://localhost:8080'
+]
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true)
+      if (allowedOrigins.indexOf(origin) === -1) {
+        // If a specific origin isn't found on the list of allowed origins
+        let message = 'The CORS policy for this application doesn\'t allow access from origin ' + origin
+        return callback(new Error(message), false)
+      }
+      return callback(null, true)
+    }
+  })
+)
+
+app.get('/', (req, res) => {
+  res.send('Welcome to my API for journaling!')
+})
 
 /**
  * ADD A USER
@@ -24,24 +51,25 @@ mongoose.connect('mongodb://localhost:27017/journaling', { useNewUrlParser: true
  *    Entries: Array
  * }
  */
-app.post('/users', async (req, res) => {
-  await Users.findOne({ UserName: req.body.UserName })
-    .then(user => {
+app.post('/users', (req, res) => {
+  console.log(req.body)
+  Users.findOne({ UserName: req.body.UserName })
+    .then((user) => {
       if (user) {
         return res.status(400).send(req.body.UserName + ' already exists')
       } else {
         Users.create({
-          UserName: req.body.UserName,
-          Password: req.body.Password,
           FirstName: req.body.FirstName,
           LastName: req.body.LastName,
+          UserName: req.body.UserName,
+          Password: req.body.Password,
           Email: req.body.Email
         })
-        .then(user => { res.status(201).json(user) })
-      .catch(err => {
-        console.error(err)
-        res.status(500).send('Error: ' + err)
-      })
+        .then((user) => { res.status(201).json(user) })
+        .catch(err => {
+          console.error(err)
+          res.status(500).send('Error: ' + err)
+        })
       }
     })
     .catch(err => {
