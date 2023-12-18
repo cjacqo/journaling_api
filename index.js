d6d3cc2841bebc51f0ab270ab47ea5cfc20e27eb
cjacqo <76092910+cjacqo@users.noreply.github.com>
2023-12-18 13:57:09 -0800

    Removed test code in index.js

--- index.js
+++ index.js
@@ -6,36 +6,7 @@ const Models = require('./models.js')
 const fs = require('fs')
 const path = require('path')
 
-let topBooks = [
-  {
-    title: 'Harry Potter and the Sorcerer\'s Stone',
-    author: 'J.K. Rowling'
-  },
-  {
-    title: 'Lord of the Rings',
-    author: 'J.R.R. Tolkien'
-  },
-  {
-    title: 'Twilight',
-    author: 'Stephanie Meyer'
-  }
-]
 
-const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), { flags: 'a' })
-app.use(morgan('combined', { stream: accessLogStream }))
-
-// GET requests
-app.get('/', (req, res) => {
-  res.send('Welcome to my book club!')
-})
-
-app.get('/documentation', (req, res) => {
-  res.sendFile('documentation.html', { root: __dirname })
-})
-
-app.get('/books', (req, res) => {
-  res.json(topBooks)
-})
 
 // list for requests
 app.listen(8080, () => {
