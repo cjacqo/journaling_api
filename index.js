ff2f4a1fc5de5e4562c086941e41a863f144e7a9
cjacqo <76092910+cjacqo@users.noreply.github.com>
2023-12-18 18:41:32 -0800

    Installed bcrypt package

--- index.js
+++ index.js
@@ -25,19 +25,17 @@ let allowedOrigins = [
   'https://localhost:8080'
 ]
 
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true)
-      if (allowedOrigins.indexOf(origin) === -1) {
-        // If a specific origin isn't found on the list of allowed origins
-        let message = 'The CORS policy for this application doesn\'t allow access from origin ' + origin
-        return callback(new Error(message), false)
-      }
-      return callback(null, true)
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true)
+    if (allowedOrigins.indexOf(origin) === -1) {
+      // If a specific origin isn't found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn\'t allow access from origin ' + origin
+      return callback(new Error(message), false)
     }
-  })
-)
+    return callback(null, true)
+  }
+}))
 
 app.get('/', (req, res) => {
   res.send('Welcome to my API for journaling!')
