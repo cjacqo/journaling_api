75cf3cc26da67414edf402f41b336ab784b96804
cjacqo <76092910+cjacqo@users.noreply.github.com>
2023-12-18 17:44:40 -0800

    Finished auth.js file and imported auth and passport into index.js

---
+++ auth.js
@@ -0,0 +1,33 @@
+const jwtSecret = 'your_jwt_secret'
+
+const jwt = require('jsonwebtoken'),
+passport = require('passport')
+
+let generateJWTToken = (user) => {
+  return jwt.sign(user, jwtSecret, {
+    subject: user.UserName,
+    expiresIn: '7d',
+    algorithm: 'HS256'
+  })
+}
+
+/* POST LOGIN */
+module.exports = (router) => {
+  router.post('/login', (req, res) => {
+    passport.authenticate('local', { session: false }, (err, user, info) => {
+      if (err || !user) {
+        return res.status(400).json({
+          message: 'Something is not right',
+          user: user
+        })
+      }
+      req.login(user, { session: false }, (err) => {
+        if (err) {
+          res.send(err)
+        }
+        let token = generateJWTToken(user.toJSON())
+        return res.json({ user, token })
+      })
+    })(req, res)
+  })
+}
--- index.js
+++ index.js
@@ -15,6 +15,11 @@ app.use(morgan('common'))
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.json())
 
+let auth = require('./auth')(app)
+
+const passport = require('passport')
+require('./passport.js')
+
 const cors = require('cors')
 let allowedOrigins = [
   'https://localhost:8080'
