9f758381b6244f07f4ad31e3c67a2bf1911aa542
cjacqo <76092910+cjacqo@users.noreply.github.com>
2023-12-20 18:01:24 -0800

    Removed all origins

--- index.js
+++ index.js
@@ -30,19 +30,17 @@ let allowedOrigins = [
   'https://127.0.0.1:5173'
 ]
 
-app.use(cors())
-
-// app.use(cors({
-//   origin: (origin, callback) => {
-//     if (!origin) return callback(null, true)
-//     if (allowedOrigins.indexOf(origin) === -1) {
-//       // If a specific origin isn't found on the list of allowed origins
-//       let message = 'The CORS policy for this application doesn\'t allow access from origin ' + origin
-//       return callback(new Error(message), false)
-//     }
-//     return callback(null, true)
-//   }
-// }))
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true)
+    if (allowedOrigins.indexOf(origin) === -1) {
+      // If a specific origin isn't found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn\'t allow access from origin ' + origin
+      return callback(new Error(message), false)
+    }
+    return callback(null, true)
+  }
+}))
 
 const { check, validationResult } = require('express-validator') 
 
