6134da80e73e737dc2fc1d16d78c63adf5c993d8
cjacqo <76092910+cjacqo@users.noreply.github.com>
2023-12-20 17:44:00 -0800

    Commented out app.use(cors())

--- index.js
+++ index.js
@@ -37,17 +37,17 @@ app.use((req, res, next) => {
   next()
 })
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin) return callback(null, true)
-    if (allowedOrigins.indexOf(origin) === -1) {
-      // If a specific origin isn't found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn\'t allow access from origin ' + origin
-      return callback(new Error(message), false)
-    }
-    return callback(null, true)
-  }
-}))
+// app.use(cors({
+//   origin: (origin, callback) => {
+//     if (!origin) return callback(null, true)
+//     if (allowedOrigins.indexOf(origin) === -1) {
+//       // If a specific origin isn't found on the list of allowed origins
+//       let message = 'The CORS policy for this application doesn\'t allow access from origin ' + origin
+//       return callback(new Error(message), false)
+//     }
+//     return callback(null, true)
+//   }
+// }))
 
 const { check, validationResult } = require('express-validator') 
 
